4df1d36026223a6ad00c0af1750bcc7114bc5fab
Aaron Schlesinger <aschlesinger@deis.com>
2016-02-12 08:52:42 -0800

    removing pointer values
    the zero values are good enough for now

--- handlers/build_bodies.go
+++ handlers/build_bodies.go
@@ -5,9 +5,9 @@ const (
 )
 
 type buildReq struct {
-	BuildImage   *string `json:"build_image"`
-	CGOEnabled   *bool   `json:"cgo_enabled"`
-	CrossCompile *bool   `json:"cross_compile"`
+	BuildImage   string `json:"build_image"`
+	CGOEnabled   bool   `json:"cgo_enabled"`
+	CrossCompile bool   `json:"cross_compile"`
 }
 
 func newBuildReq() *buildReq {
@@ -16,21 +16,23 @@ func newBuildReq() *buildReq {
 
 func (b *buildReq) buildImage() string {
 	ret := defaultBuildImg
-	if b.BuildImage != nil {
-		ret = *b.BuildImage
+	if b.BuildImage != "" {
+		ret = b.BuildImage
 	}
 	return ret
 }
 
 func (b *buildReq) envs() []string {
 	var env []string
-	if b.CGOEnabled == nil || !*b.CGOEnabled {
+	if !b.CGOEnabled {
 		env = append(env, "CGO_ENABLED=0")
 	} else {
 		env = append(env, "CGO_ENABLED=1")
 	}
-	if b.CrossCompile != nil && *b.CrossCompile {
+	if b.CrossCompile {
 		env = append(env, "CROSS_COMPILE=1")
+	} else {
+		env = append(env, "CROSS_COMPILE=0")
 	}
 	return env
 }
